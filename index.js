a4cbf676bdcf388c471cfe85918b38c68c399815
aigul1981 <749021@mail.ru>
2021-06-16 17:48:18 +0300

    js_16.06

---
+++ index.js
@@ -0,0 +1,20 @@
+let userName = "aigul";
+alert(userName);
+
+let userAge = 40;
+alert(userAge);
+
+let a = 100;
+let b = 200;
+let temp = a; //100
+alert("Значение a: " + a);
+alert("Значение b: " + b);
+
+a = b; //200
+b = temp; //100
+
+// об а = 1
+// об б = 2
+
+alert("Значение a: " + a);
+alert("Значение b: " + b);
